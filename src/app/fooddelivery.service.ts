f333762be95243fd415fa417fbac4dd81174401d
Roniya Rojan <roniyarojan24@gmail.com>
2020-05-28 11:58:52 -0400

    API key added

--- src/app/fooddelivery.service.ts
+++ src/app/fooddelivery.service.ts
@@ -5,5 +5,9 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root",
 })
 export class FooddeliveryService {
+  APP_KEY: string = "3a7b094e0a14d79607405c98cfeaaec5	";
+  APP_ID: string = "16d80f85";
+  favorites: any[] = [];
+  selectedFood: any;
   constructor(private http: HttpClient) {}
 }
