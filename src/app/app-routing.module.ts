3ed6f2dbe562cda9e4f6239abeb130a749c7ab04
Roniya Rojan <roniyarojan24@gmail.com>
2020-05-28 11:28:36 -0400

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { AboutusComponent } from "./aboutus/aboutus.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "sign-up", component: SignUpComponent },
+  { path: "aboutus", component: AboutusComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "**", redirectTo: "/home", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
